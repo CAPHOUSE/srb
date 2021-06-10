9895059a5863b35ee3212af47781aeff1954305f
chen <2305211762@qq.com>
2021-06-10 09:59:19 +0800

    init project

---
+++ service-core/src/main/java/com/atguigu/srb/core/controller/api/BorrowerController.java
@@ -0,0 +1,36 @@
+package com.atguigu.srb.core.controller.api;
+
+import com.atguigu.common.result.Result;
+import com.atguigu.srb.core.pojo.vo.BorrowerVO;
+import com.atguigu.srb.core.service.BorrowerService;
+import com.atguigu.srb.util.JwtUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Api(tags = "借款人")
+@RestController
+@RequestMapping("/api/core/borrower")
+@Slf4j
+public class BorrowerController {
+
+    @Autowired
+    private BorrowerService borrowerService;
+
+
+    @ApiOperation("保存借款人信息")
+    @PostMapping("/auth/save")
+    public Result save(@RequestBody BorrowerVO borrowerVO, HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        borrowerService.saveBorrowerVOByUserId(borrowerVO, userId);
+        return Result.ok().message("信息提交成功");
+    }
+}
--- service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowerServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowerServiceImpl.java
@@ -1,10 +1,20 @@
 package com.atguigu.srb.core.service.impl;
 
+import com.atguigu.srb.core.enums.BorrowerStatusEnum;
+import com.atguigu.srb.core.mapper.BorrowerAttachMapper;
+import com.atguigu.srb.core.mapper.UserInfoMapper;
 import com.atguigu.srb.core.pojo.entity.Borrower;
 import com.atguigu.srb.core.mapper.BorrowerMapper;
+import com.atguigu.srb.core.pojo.entity.BorrowerAttach;
+import com.atguigu.srb.core.pojo.entity.UserInfo;
+import com.atguigu.srb.core.pojo.vo.BorrowerVO;
 import com.atguigu.srb.core.service.BorrowerService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +27,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class BorrowerServiceImpl extends ServiceImpl<BorrowerMapper, Borrower> implements BorrowerService {
 
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @Autowired
+    private BorrowerAttachMapper borrowerAttachMapper;
+
+    @Override
+    public void saveBorrowerVOByUserId(BorrowerVO borrowerVO, Long userId) {
+
+//       获取用户基本信息
+        UserInfo userInfo = userInfoMapper.selectById(userId);
+
+//        保存借款人信息
+        Borrower borrower = new Borrower();
+        BeanUtils.copyProperties(borrowerVO,borrower);
+        borrower.setUserId(userId);
+        borrower.setName(userInfo.getName());
+        borrower.setIdCard(userInfo.getIdCard());
+        borrower.setMobile(userInfo.getMobile());
+        borrower.setStatus(BorrowerStatusEnum.AUTH_RUN.getStatus());  //认证中
+
+        baseMapper.insert(borrower);
+
+//        保存附件
+        List<BorrowerAttach> borrowerAttachList = borrowerVO.getBorrowerAttachList();
+        for (BorrowerAttach borrowerAttach : borrowerAttachList) {
+            borrowerAttach.setBorrowerId(borrower.getId());
+            borrowerAttachMapper.insert(borrowerAttach);
+        }
+
+//      更新userInfo中的借款人认证状态
+        userInfo.setBindStatus(BorrowerStatusEnum.AUTH_RUN.getStatus());
+        userInfoMapper.updateById(userInfo);
+    }
 }
