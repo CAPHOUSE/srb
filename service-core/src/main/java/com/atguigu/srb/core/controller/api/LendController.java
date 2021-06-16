712a6adb1e2e159fe451ffc7ff34c326109d1a20
chen <2305211762@qq.com>
2021-06-16 08:30:01 +0800

    init project

---
+++ service-core/src/main/java/com/atguigu/srb/core/controller/api/LendController.java
@@ -0,0 +1,45 @@
+package com.atguigu.srb.core.controller.api;
+
+
+import com.atguigu.common.result.Result;
+import com.atguigu.srb.core.pojo.entity.Lend;
+import com.atguigu.srb.core.service.LendService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 标的准备表 前端控制器
+ * </p>
+ *
+ * @author Helen
+ * @since 2021-02-20
+ */
+
+@Api(tags = "标的")
+@RestController
+@RequestMapping("/api/core/lend")
+@Slf4j
+public class LendController {
+
+    @Autowired
+    private LendService lendService;
+
+
+    @ApiOperation("获取标的列表")
+    @GetMapping("/list")
+    public Result list(){
+        List<Lend> lendList = lendService.selectList();
+        return Result.ok(lendList);
+    }
+
+}
+
